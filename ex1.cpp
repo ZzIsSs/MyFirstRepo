eed93742d2b8b4304765f2de5e80ac5ac7e1edd0
ZzIsS <tintin082006@gmail.com>
2025-03-26 18:21:49 +0700

    Update ex1.cpp

--- ex1.cpp
+++ ex1.cpp
@@ -8,22 +8,22 @@ int linearSearch(int* nums, int numsSize, int key) {
 	}return -1;
 }
 
-//int* CreateArray(int& n) {
-//	cout << "Nhap so luong phan tu ";
-//	cin >> n;
-//	cout << endl;
-//	int* ans = new int[n];
-//	for (int i = 0; i < n; i++) {
-//		cout << "Hay nhap phan tu thu " << i << endl;
-//		cin >> ans[i];
-//	}return ans;
-//}
+int* CreateArray(int& n) {
+	cout << "Nhap so luong phan tu ";
+	cin >> n;
+	cout << endl;
+	int* ans = new int[n];
+	for (int i = 0; i < n; i++) {
+		cout << "Hay nhap phan tu thu " << i << endl;
+		cin >> ans[i];
+	}return ans;
+}
 
 
-//int main() {
-//	int n;
-//	int* a = CreateArray(n);
-//	int key; cout << "Hay nhap so can tim " << endl; cin >> key;
-//	cout << "phan tu co gia tri 7 dung o vi tri " << linearSearch(a, n, 7);
-//	return 0;
-//}
+int main() {
+	int n;
+	int* a = CreateArray(n);
+	int key; cout << "Hay nhap so can tim " << endl; cin >> key;
+	cout << "phan tu dung o vi tri " << linearSearch(a, n, key);
+	return 0;
+}
